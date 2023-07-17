6a3f1de257266dd491ca26f496778ab500734f90
greysoh <dev@hofers.cloud>
2023-07-17 00:01:10 -0400

    feature: Adds Nintendo layout.

--- better_button.py
+++ better_button.py
@@ -1,7 +1,11 @@
 import threading
 import time
 
+from config import use_nintendo_layout
 import nxbt
+
+key_bign_raw = ["A", "B", "X", "Y"]
+key_bign_layout = ["B", "A", "Y", "X"]
 
 class BetterButton: 
   def __init__(self, is_pro_controller = True, controller_index = 0, nxbt = nxbt.Nxbt()):
@@ -23,6 +27,10 @@ class BetterButton:
     for key in key_list:
       if key == "L_STICK_PRESS" or key == "R_STICK_PRESS":
         self.base_packet[f"{key[0]}_STICK"]["PRESSED"] = True
+        break
+      elif key in key_bign_raw and use_nintendo_layout:
+        self.base_packet[key_bign_layout[key_bign_raw.index(key)]] = True
+        break
       
       self.base_packet[key] = True
   
@@ -30,6 +38,9 @@ class BetterButton:
     for key in key_list:
       if key == "L_STICK_PRESS" or key == "R_STICK_PRESS":
         self.base_packet[f"{key[0]}_STICK"]["PRESSED"] = False
+      elif key in key_bign_raw and use_nintendo_layout:
+        self.base_packet[key_bign_layout[key_bign_raw.index(key)]] = True
+        break
       
       self.base_packet[key] = False
 
