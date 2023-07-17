0e95d71a79c5653c124c8bfb45fca6f62e90bb47
greysoh <dev@hofers.cloud>
2023-07-17 11:43:20 -0400

    chore: Initializes desktop.py. (2/2)

---
+++ index.py
@@ -0,0 +1,7 @@
+import os
+
+print("Oooops: You are running the old controller passthrough program.")
+print("This has been moved to controller.py. Starting...")
+print("\nNOTE: This stub will be removed in a later update.")
+
+os.system("python controller.py")
