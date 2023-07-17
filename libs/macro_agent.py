dde1cfc8d95eec56cfac71743848afc72ba8d3af
greysoh <dev@hofers.cloud>
2023-07-17 15:25:24 -0400

    feature: Implement base macros.
    Next commit I'll probably code precaching.

---
+++ libs/macro_agent.py
@@ -0,0 +1,115 @@
+import kdl
+
+class MacroAgent:
+  def __init__(self, str):
+    self.keybinding_cache = []
+    self.macro_cache = []
+
+    self.doc = kdl.parse(str)
+  
+  def get_keybinding(self, key_to_search):
+    desktop_binding_cache_search = [x for x in self.keybinding_cache if x["key"] == key_to_search]
+    desktop_bindings_lists = [x for x in self.doc.nodes if x.name == "desktop_bindings"]
+   
+    if len(desktop_binding_cache_search) != 0:
+      print("USING CACHE.")
+      return desktop_binding_cache_search[0]["value"]
+   
+    for desktop_list in desktop_bindings_lists:
+      for binding in desktop_list.nodes:
+        gen_binding_info = {
+          "key": None,
+          "value": None,
+        }
+
+        arguments = list(binding.props.items())
+
+        for key, value in arguments:
+          if key == "key":
+            gen_binding_info["key"] = value
+          elif key == "button":
+            gen_binding_info["value"] = value
+        
+        if key_to_search == gen_binding_info["key"] and gen_binding_info["value"] != None:
+          self.keybinding_cache.append(gen_binding_info)
+          return gen_binding_info["value"]
+  
+  def get_macro(self, type, key):
+    macros_cache_search = [x for x in self.macro_cache if x["mode"] == type and x["key"] == key]
+    macros_lists = [x for x in self.doc.nodes if x.name == "macros"]
+
+    for node in macros_lists:
+      mode_lists = [x for x in node.nodes if x.name == type]
+
+      for mode_items in mode_lists:
+        for macros in mode_items.nodes:
+          if macros.name != key:
+            continue
+
+          time_scale = 1 
+          macro_items = []
+
+          for macro_key in macros.nodes:
+            arguments_tuple = tuple(macro_key.props.items())
+            arguments = dict((x, y) for x, y in arguments_tuple)
+
+            match macro_key.name:
+              case "press":
+                duration = arguments["duration"]
+                key = arguments["key"]
+
+                if len(arguments) > 2:
+                  time_scale = arguments["timeScale"]
+                  if time_scale == "ms":
+                    duration = duration / 1000
+                
+                macro_items.append({
+                  "type": "key_down",
+                  "key": key
+                })
+
+                macro_items.append({
+                  "type": "sleep",
+                  "time": duration
+                })
+
+                macro_items.append({
+                  "type": "key_up",
+                  "key": key
+                })
+
+                break
+            
+              case "sleep":
+                macro_items.append({
+                  "type": "sleep",
+                  "time": arguments["duration"]
+                })
+              
+              case "key_down":
+                macro_items.append({
+                  "type": "key_down",
+                  "key": arguments["key"]
+                })
+              
+              case "key_up":
+                macro_items.append({
+                  "type": "key_up",
+                  "key": arguments["key"]
+                })
+
+          self.macro_cache.append({
+            "mode": type,
+            "key": key,
+            "macro_items": macro_items
+          })
+
+          return macro_items
+
+  KEYBOARD = "keyboard"
+  CONTROLLER = "controller"
+
+class MacroAgentFromFile(MacroAgent):
+  def __init__(self, file_path):
+    with open(file_path) as f:
+      super().__init__(f.read())
